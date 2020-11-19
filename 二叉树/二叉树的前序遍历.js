964f025db95b5ccbfca4cf1040a7b10bf2a54416
lzt <lzt@zhutxia.com>
2020-11-19 15:50:28 +0800

    feat: 二叉树前序遍历-遍历解法

--- 二叉树/二叉树的前序遍历.js
+++ 二叉树/二叉树的前序遍历.js
@@ -20,8 +20,10 @@ const binaryTree = {
 /**
  * @param {TreeNode} root
  * @return {number[]}
+ * 递归解法
  */
 const preorderTraversal = function(root) {
+  if (!root) return [];
   let result = []
   let preorderTraversalNode = (node) => {
       if (node) {
@@ -35,6 +37,30 @@ const preorderTraversal = function(root) {
   }
   preorderTraversalNode(root)
   console.log(result)
+  return result
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ * 遍历解法
+ */
+const preorderTraversal1 = function(root) {
+  if (!root) return [];
+  let result = []
+  let stack = [root]
+  while (stack.length) {
+    let size = stack.length
+    while (size--) {
+      let node = stack.pop()
+      // 先取根节点
+      result.push(node.val)
+      // 我们先打印左子树，然后右子树
+      // 所以先加入栈的是右子树，然后左子树
+      if (node.right !== null) stack.push(node.right)
+      if (node.left !== null) stack.push(node.left)
+    }
+  }
   return result
 }
 
