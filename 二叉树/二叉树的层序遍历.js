539a8c22369c60848bcc8449f9f68a1b99ca4323
lzt <lzt@zhutxia.com>
2020-11-23 09:24:59 +0800

    init：二叉树的层序遍历

---
+++ 二叉树/二叉树的层序遍历.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+const levelOrder = function(root) {
+  const res = []
+  if (!root) return res
+
+  const queue = []
+  queue.push(root)
+  while(queue.length) {
+    // 二叉树当前层的节点数
+    const currentLevelSize = queue.length
+    // 遍历当前层节点并保存到数组中
+    for (let i = 0; i < currentLevelSize; i++) {
+      let node = queue.shift()
+      res[res.length - 1].push(node.val)
+      if (node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right)
+    }
+  }
+
+  return res
+}
